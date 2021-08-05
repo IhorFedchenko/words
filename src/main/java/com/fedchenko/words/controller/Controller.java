0c2de74bfafb5cfdb8ee5b5112ed3f471d39ad71
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2021-08-06 00:18:33 +0300

    !@! parse JSON array and set Java object

--- src/main/java/com/fedchenko/words/controller/Controller.java
+++ src/main/java/com/fedchenko/words/controller/Controller.java
@@ -3,16 +3,34 @@ package com.fedchenko.words.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fedchenko.words.CheckerWords;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Iterator;
 
 @RestController
 public class Controller {
     @PostMapping("/words")
     public String postBody(@RequestBody String request) {
-//        TODO parse JSON and set to java object
 //        TODO return Java object like as JSON
-
-        return "";
+        CheckerWords words = new CheckerWords();
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = new JSONObject();
+        try {
+             jsonObject =  (JSONObject) jsonParser.parse(request);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        JSONArray jsonArray = (JSONArray) jsonObject.get("words");
+        Iterator<String> iterator = jsonArray.iterator();
+        while (iterator.hasNext()){
+            words.setWord(iterator.next());
+        }
+        return words.getWords().toString();
     }
 
 }
