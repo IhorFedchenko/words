aff1fe7fff900b7d626403dbeb11299a4eb77f08
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2021-08-09 11:36:47 +0300

    add WordsReslfulService and make slim controller

---
+++ src/main/java/com/fedchenko/words/WordsRestfulService.java
@@ -0,0 +1,54 @@
+package com.fedchenko.words;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.Iterator;
+
+public class WordsRestfulService {
+
+    private CheckerWords words;
+    private JSONParser jsonParser;
+    private JSONObject jsonObject;
+    private JSONArray jsonArray;
+
+    String sufix = "}";
+
+    public WordsRestfulService() {
+        words = new CheckerWords();
+        jsonParser = new JSONParser();
+        jsonObject = new JSONObject();
+    }
+
+    public void getRequest(String request) {
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(request);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        jsonArray = (JSONArray) jsonObject.get("words");
+    }
+
+    public String getResponse(){
+        Iterator<String> iterator = jsonArray.iterator();
+        while (iterator.hasNext()){
+            words.setWord(iterator.next());
+        }
+        jsonArray.clear();
+        jsonArray.addAll(words.getWords());
+        return getArrayName() + jsonArray.toString() + sufix;
+    }
+private String getArrayName(){
+        String arrayName = new String();
+        String stringJSONObject = jsonObject.toJSONString();
+        if (stringJSONObject.startsWith("{") && stringJSONObject.endsWith("}") && stringJSONObject.contains(":")){
+            int indexOf = stringJSONObject.indexOf(":");
+            indexOf++;
+            arrayName = stringJSONObject.substring(0, indexOf);
+        }
+        return arrayName;
+}
+
+}
--- src/main/java/com/fedchenko/words/controller/Controller.java
+++ src/main/java/com/fedchenko/words/controller/Controller.java
@@ -1,44 +1,15 @@
 package com.fedchenko.words.controller;
 
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fedchenko.words.CheckerWords;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.fedchenko.words.WordsRestfulService;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Iterator;
 
 @RestController
 public class Controller {
     @PostMapping("/words")
     public String postBody(@RequestBody String request) {
-        String prefix = "{\"words\": ";
-        String sufix = "}";
-//        TODO make slim controller, all below like as class with arg
-//        TODO сделать небольшой парсер json, если начинается на { и заканчивается }
-//        test newBranch
-// взять слово до скобок и удалить кавычки, метод взять имя массива
-        CheckerWords words = new CheckerWords();
-        JSONParser jsonParser = new JSONParser();
-        JSONObject jsonObject = new JSONObject();
-//       String nameArray = jsonObject *how get nameArray
-        try {
-             jsonObject =  (JSONObject) jsonParser.parse(request);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        JSONArray jsonArray = (JSONArray) jsonObject.get("words");
-        Iterator<String> iterator = jsonArray.iterator();
-        while (iterator.hasNext()){
-            words.setWord(iterator.next());
-        }
-        jsonArray.clear();
-        jsonArray.addAll(words.getWords());
-        return prefix + jsonArray.toString() + sufix;
+        WordsRestfulService wordsRestfulService = new WordsRestfulService();
+        wordsRestfulService.getRequest(request);
+        return wordsRestfulService.getResponse();
     }
 
 }
