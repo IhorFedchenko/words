019d2b2e534ec7b6184631c0882d389a6102286d
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2021-08-12 12:19:27 +0300

    #after code review# fix hitCounter, del getFirst(Last)Char method

--- src/main/java/com/fedchenko/words/CheckerWords.java
+++ src/main/java/com/fedchenko/words/CheckerWords.java
@@ -1,7 +1,5 @@
 package com.fedchenko.words;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +8,6 @@ public class CheckerWords {
 
     public List<String> getWords() {
         List<String> result = new ArrayList<>(words.subList(0, hitCounter()));
-
         return result;
     }
 
@@ -18,22 +15,11 @@ public class CheckerWords {
         words.add(input);
     }
 
-    private String getFirstChar(String input) {
-        String result = new String();
-        return result.valueOf(input.charAt(0));
-    }
-
-    private String getLastChar(String input) {
-        String result = new String();
-        int size = input.length();
-        return result.valueOf(input.charAt(size - 1));
-    }
-
     private int hitCounter() {
         int result = 1;
         for (int i = 0; i < words.size() - 1; i++) {
             try {
-                if (getLastChar(words.get(i)).equals(getFirstChar(words.get(i + 1)))) {
+                if (words.get(i).endsWith(String.valueOf(words.get(i + 1).charAt(0)))) {
                     result++;
                 } else break;
             } catch (IndexOutOfBoundsException e) {
